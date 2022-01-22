4649ea2623f35fcf3d6217e23d80cfdf5e314478
Tiberiu <lordiulian@gmail.com>
2022-01-22 03:11:57 +0100

    calendar added

--- src/App/Pages/Calendar/CalendarPage.js
+++ src/App/Pages/Calendar/CalendarPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 class CalendarPage extends React.Component {
 
@@ -12,9 +14,21 @@ class CalendarPage extends React.Component {
     }
 
     render() {
+
+        let dHeight = 700;
+        if (window.innerWidth < 768) dHeight = 500;
+
         return (
-            <div>
-                NOTHING HERE...
+            <div className="col-12 bg-white shadow-sm p-3 mth_calendar">
+                <FullCalendar
+                    plugins={[dayGridPlugin]}
+                    initialView="dayGridMonth"
+                    events={[
+                        { title: 'Buy BABA Shares', date: '2022-01-22' },
+                        { title: 'Sell BABA Shares', date: '2022-01-29', backgroundColor: '#ff5555', borderColor: '#ff5555' }
+                    ]}
+                    contentHeight={dHeight}
+                />
             </div>
         );
     }
